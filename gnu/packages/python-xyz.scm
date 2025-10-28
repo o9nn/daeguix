c45e622fe9f1b49b68695f9c144f259edb1fd73f
Nicolas Graves <ngraves@ngraves.fr>
2025-10-28 23:43:29 +0100

    gnu: python-watchfiles: Remove coverage inputs.
    * gnu/packages/python-xyz.scm (python-watchfiles)[native-inputs]:
    Remove python-coverage, python-pytest-cov.
    
    Change-Id: Id7ce71eb87aa032502657bfaf78650ba8c9301f9
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -40088,10 +40088,8 @@ but portable.")
      (append
       (list maturin
             python-anyio
-            python-coverage
             python-dirty-equals
             python-pytest
-            python-pytest-cov
             python-pytest-mock
             python-pytest-timeout
             rust
